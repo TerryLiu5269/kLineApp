438366ca2238684a2d053efaac57afcc16c2409e
Terry.liu <terry.liu@collaborate.tw>
2024-10-27 20:31:11 +0800

    Auto-generated commit

--- main-WHHP5WUL.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./chunk-KK2UHRIC.js";import{sa as p,ta as r,xa as t}from"./chunk-JZ3443HY.js";var e=[{path:"",loadChildren:()=>import("./chunk-OLIMVROR.js").then(o=>o.MainPageModule)},{path:"drilldown/:symbol",loadChildren:()=>import("./chunk-2235ZBPH.js").then(o=>o.DrilldownPageModule)}];var m={providers:[t(e),p()]};r(i,m).catch(o=>console.error(o));
